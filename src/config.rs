10794d373e7f643ce5d507d065b4bec4c44dd121
Rongcui Dong <rongcuid@outlook.com>
2023-07-12 13:15:00 -0400

    Multiple level inputs

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 use clap::Args;
 use tracing::Level;
 use tracing_indicatif::IndicatifLayer;
-use tracing_subscriber::prelude::*;
+use tracing_subscriber::{prelude::*, EnvFilter};
 
 pub trait HasRuscConfig {
     fn rusc_config<'a>(&'a self) -> &'a RuscConfig;
@@ -16,13 +16,19 @@ pub struct RuscConfig {
 impl RuscConfig {
     pub fn init(&self) {
         let indicatif_layer = IndicatifLayer::new();
-        let max_level = match self.verbose {
+        let env = EnvFilter::from_default_env();
+        let cli_max_level = match self.verbose {
             0 => Level::WARN,
             1 => Level::INFO,
             2 => Level::DEBUG,
             _ => Level::TRACE,
         };
+        let max_level = env
+            .max_level_hint()
+            .and_then(|l| l.into_level())
+            .map_or(cli_max_level, |l| l.max(cli_max_level));
         tracing_subscriber::registry()
+            .with(EnvFilter::from_default_env())
             .with(
                 tracing_subscriber::fmt::layer()
                     .without_time()
